3ab3ae9ccf1b1577334826c33ea16e9c12c5866a
schertov-comprehend <schertov@comprehend.com>
2019-01-21 00:01:32 +0300

    Initial docker &  frontend
    Empty React frontend added
    Docker plugin added

--- LetsFly/build.sbt
+++ LetsFly/build.sbt
@@ -3,3 +3,11 @@ name := "LetsFly"
 version := "0.1"
 
 scalaVersion := "2.11.12"
+
+enablePlugins(JavaAppPackaging)
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.5.13",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.13",
+  "com.typesafe.akka" %% "akka-http" % "10.1.3"
+)
---
+++ LetsFly/src/main/scala/Server.scala
@@ -0,0 +1,30 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.server.Directives._
+
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
+
+object Server extends App {
+  //Init
+  val host = "0.0.0.0"
+  val port = 9000
+
+  implicit val system: ActorSystem = ActorSystem("helloworld")
+  implicit val executor: ExecutionContext = system.dispatcher
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  def route = path("hello") {
+    get {
+      complete("Hello, World!")
+    }
+  }
+  val bindingFuture = Http().bindAndHandle(route, host, port)
+
+  bindingFuture.onComplete {
+    case Success(serverBinding) => println(s"listening to ${serverBinding.localAddress}")
+    case Failure(error) => println(s"error: ${error.getMessage}")
+  }
+
+}

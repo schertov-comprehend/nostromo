ad1795d063dc67263c779566039c4bbe34be4030
schertov-comprehend <schertov@comprehend.com>
2019-01-21 14:27:45 +0300

    post support
    Post support and json unmarshalling added

--- LetsFly/build.sbt
+++ LetsFly/build.sbt
@@ -7,7 +7,8 @@ scalaVersion := "2.11.12"
 enablePlugins(JavaAppPackaging)
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.5.13",
-  "com.typesafe.akka" %% "akka-stream" % "2.5.13",
-  "com.typesafe.akka" %% "akka-http" % "10.1.3"
+  "com.typesafe.akka" %% "akka-actor" % "2.5.19",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.19",
+  "com.typesafe.akka" %% "akka-http" % "10.1.3",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
 )
--- LetsFly/src/main/scala/Server.scala
+++ LetsFly/src/main/scala/Server.scala
@@ -2,22 +2,40 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
+import spray.json._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 
 import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success}
 
-object Server extends App {
+final case class Name(name: String)
+
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val nameFormat = jsonFormat1(Name)
+}
+
+object Server extends App with JsonSupport {
   //Init
   val host = "0.0.0.0"
   val port = 9000
 
-  implicit val system: ActorSystem = ActorSystem("helloworld")
+
+  implicit val system: ActorSystem = ActorSystem("nostromo")
   implicit val executor: ExecutionContext = system.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   def route = path("hello") {
     get {
+      println("Got GET")
       complete("Hello, World!")
+    } ~
+    post {
+      println("Got POST")
+      complete("U r fine")
+      entity(as[Name]) {
+          item => complete("OK. U r fine: " + item.name)
+      }
+
     }
   }
   val bindingFuture = Http().bindAndHandle(route, host, port)

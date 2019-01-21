133477c370e3e36f6c07e54a1b6b0734e677507d
schertov-comprehend <schertov@comprehend.com>
2019-01-21 18:30:21 +0300

    minor stuff
    Minor naming changes

--- myfront/src/App.js
+++ myfront/src/App.js
@@ -3,17 +3,17 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: 'z', hey:''}
+    this.state = { name: '', hey:''}
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 	handleChange(event) {
-	  this.setState({value: event.target.value})
+	  this.setState({name: event.target.value})
 	}
 	handleSubmit(event) {
 	    event.preventDefault();
-	    this.setState ({hey:'Hello ' + this.state.value});
+	    this.setState ({hey:'Hello ' + this.state.name});
   	}
 	render() {
 	  return (
@@ -21,9 +21,9 @@ class App extends Component {
 	     <div>
              <form onSubmit={this.handleSubmit}>
         	<label>Name:
-          		<input value={this.state.value} onChange={this.handleChange} />
+          		<input id="name" value={this.state.value} onChange={this.handleChange} />
         	</label>
-        	<input type="submit" value="Submit" />
+        	<input type="submit" value="Go" />
       	     </form>
              </div>
 	     <div><h1>{this.state.hey}</h1></div>
